db97d1a598632e28259fba68017306bd0ac17e04
Sankaranarayanankm <sankaranarayanan.km97@gmail.com>
2023-12-04 12:36:52 +0530

    added styling as per trainer

--- src/Component/Layout/Header.js
+++ src/Component/Layout/Header.js
@@ -1,15 +1,18 @@
 import React from "react";
-import "./Header.css"
-const Header = () => {
+import classes from "./Header.module.css"
+import mealsImage from "../../assests/premium_photo-1661369889067-c86c31362f83.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+const Header = (props) => {
   return (
-    <nav className="header__nav">
-      <h1 className="header__h1">ReactMeals</h1>
-      <div className="header__div">
-
-      <div >Your Cart</div>
-      <button className="header__button">0</button>
+   <React.Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="table full of delicious food!"/>
       </div>
-    </nav>
+   </React.Fragment>
   );
 };
 
